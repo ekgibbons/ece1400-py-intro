f92cfae141a40101dc4fff05110e6cb8693e489e
Eric Gibbons <ericgibbons@weber.edu>
2023-08-31 15:09:31 -0600

    initialized repo

---
+++ testintro.py
@@ -0,0 +1,134 @@
+import io
+import contextlib
+import random
+import subprocess
+import unittest
+
+import numpy as np
+
+import introsol
+import sphere
+
+class TestIntro(unittest.TestCase):
+
+    def test_hello(self):
+
+        print("\ntesting helloworld.py")
+        
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            introsol.hello_world()
+        hello_string_sol = f.getvalue().strip()
+
+        result = subprocess.run(["python","helloworld.py"],
+                                stdout=subprocess.PIPE)
+        hello_string = result.stdout.decode("UTF-8").strip()
+        
+        self.assertEqual(hello_string_sol, hello_string)
+
+    def test_wheel(self):
+
+        print("\ntesting wheel.py")
+
+        with open("wheel.py", "r") as file:
+            content = file.read()
+
+        hard_code_check = False
+        # check if string present or not
+        if "190.44" in content:
+            hard_code_check = True
+
+        self.assertFalse(hard_code_check,
+                         "You are illegally hard "
+                         "coding the answer")
+
+
+        
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            introsol.wheel()
+        wheel_string_sol = f.getvalue().strip()
+
+        
+        result = subprocess.run(["python","wheel.py"],
+                                stdout=subprocess.PIPE)
+        wheel_string = result.stdout.decode("UTF-8").strip()
+        
+        self.assertEqual(wheel_string_sol, wheel_string)
+
+
+    def test_flux(self):
+
+        print("\ntesting flux.py")
+
+        with open("flux.py", "r") as file:
+            content = file.read()
+
+        hard_code_check = False
+        # check if string present or not
+        if "0.0032031" in content:
+            hard_code_check = True
+
+        self.assertFalse(hard_code_check,
+                         "You are illegally hard "
+                         "coding the answer")
+
+
+        
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            introsol.flux()
+        flux_string_sol = f.getvalue().strip()
+
+        
+        result = subprocess.run(["python","flux.py"],
+                                stdout=subprocess.PIPE)
+        flux_string = result.stdout.decode("UTF-8").strip()
+        
+        self.assertEqual(flux_string_sol, flux_string)
+
+
+    def test_sphere_volume(self):
+
+        print("\ntesting sphere.sphere_volume()")
+
+        radius = 10*random.random()
+        sol = introsol.sphere_volume(radius)
+        sub = sphere.sphere_volume(radius)
+
+        self.assertEqual(sol, sub)
+
+
+    def test_sphere_area(self):
+
+        print("\ntesting sphere.sphere_area()")
+
+        radius = 10*random.random()
+        sol = introsol.sphere_area(radius)
+        sub = sphere.sphere_area(radius)
+
+        self.assertEqual(sol, sub)
+
+    def test_sphere_script(self):
+
+        print("\ntesting sphere script.py")
+
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            introsol.sphere_script()
+        sphere_string_sol = f.getvalue().strip()
+
+        
+        result = subprocess.run(["python","sphere.py"],
+                                stdout=subprocess.PIPE)
+        sphere_string = result.stdout.decode("UTF-8").strip()
+        
+        self.assertEqual(sphere_string_sol, sphere_string)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
